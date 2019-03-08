24d45ff027b53c9fd4accd68f28c6ab7610883d4
Vijayanand Settin <stvijayanand@hotmail.com>
2019-03-08 14:57:48 -0800

    feat: getSate subscribe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//pure function
+function todos(state, action) {
+  if (action.type === "ADD_TODO") {
+    return state.concat([action.todo]);
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//factory function that creates store objects
+function createStore() {
+  /*
+    The store should have 4 parts
+    1. The state
+    2. Get the state
+    3. Listen to changes on the state
+    4. Update the state
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    */
+
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const subscribe = listener => {
+    listeners.push(listener);
+
+    //unsubscribe
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  return {
+    getState,
+    subscribe
+  };
+}
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
