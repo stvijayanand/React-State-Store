b63d27cf909f9df93485c8b748122a14b6e527fa
Vijayanand Settin <stvijayanand@hotmail.com>
2019-03-18 14:49:04 -0700

    feat: adding goals reducer

--- src/index.js
+++ src/index.js
@@ -62,6 +62,18 @@ function todos(state = [], action) {
   }
 }
 
+//Another reducer
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
 const store = createStore(todos);
 
 store.subscribe(() => {
